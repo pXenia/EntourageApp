4b4adc610d0b354fc85a25b0f9fccdcdb56d05d1
Xenia_Popova <p961083@gmail.com>
2026-02-07 20:39:57 +0300

    Добавлены шрифты и цвета

---
+++ core/ui/src/commonMain/kotlin/Colors.kt
@@ -0,0 +1,11 @@
+package com.entourageapp.core.ui
+
+import androidx.compose.ui.graphics.Color
+
+
+val EntourageBlack = Color(0xFF131516)
+val EntourageWhite = Color(0xFFFFFFFF)
+val EntourageLightBlueGray = Color(0xFFCBD3E6)
+val EntourageTeal = Color(0xFF296F7C)
+val EntouragePeach = Color(0xFFF3A989)
+val EntourageRed = Color(0xFFAB241A)
